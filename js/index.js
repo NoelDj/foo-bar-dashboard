0a75a3ffda1706454d411c65e8f838cc65246d76
noel0045 <noel0045@stud.kea.dk>
2021-08-17 21:23:12 +0200

    improvements

--- js/index.js
+++ js/index.js
@@ -5,9 +5,14 @@ import Toastify from 'toastify-js'
 import "toastify-js/src/toastify.css"
 import Chart from 'chart.js/auto'
 import '@fortawesome/fontawesome-free/js/all.min.js'
+import EditorJS from '@editorjs/editorjs'
 
 
 window.addEventListener('DOMContentLoaded', init)
+
+/* window.addEventListener('DOMContentLoaded', setTimeout(() => {
+    init()
+}, 5000)) */
 
 function init() {
     loadData()
@@ -20,51 +25,78 @@ setInterval(() => {
     loadData()
 }, 1000); */
 
-async function loadData()
-{
-  let response = await fetch('https://foobardata.herokuapp.com');
-  let data = await response.json()
-  handleData(data)
+async function loadData() {
+    let response = await fetch('https://foobardata.herokuapp.com');
+    let data = await response.json()
+    handleData(data)
 }
 
-function handleData(data){
+function handleData(data) {
 
     createLists(data)
 
-    insertData('current-orders' , data.queue.length)
-    insertData('staff' , data.bartenders.length)
+    insertData('current-orders', data.queue.length)
+    insertData('staff', data.bartenders.length)
     insertData('low-taps', getTap(data.taps))
     insertStock('stock', getStock(data.storage))
 
 
 
-    switch(data.queue.length) {
+    switch (data.queue.length) {
         case 1:
             showNotification('There is 1 order')
-          break;
+            break;
         case 0:
             showNotification(`The are no orders`)
-          break;
+            break;
         default:
             showNotification(`There are ${data.queue.length} orders`)
     }
 
-    switch(data.bartenders.length) {
+    switch (data.bartenders.length) {
         case 1:
             showNotification('There is 1 employee at work')
-          break;
+            break;
         case 0:
             showNotification(`There is no one at work`)
-          break;
+            break;
         default:
             showNotification(`There are ${data.bartenders.length} eomployees at work`)
     }
 
+    editorJsFunction()
+    removeSpinners()
+
+}
+let savedData = ''
+function removeSpinners() {
+    document.querySelectorAll('.lds-ellipsis').forEach(spinner => {
+        spinner.remove()
+    })
+}
+function editorJsFunction() {
+    const editor = new EditorJS({
+
+        holderId: 'editorjs',
+        placeholder: 'Let`s write an awesome story!',
+        data: {
+            'dasda': 21
+        }
+
+
+    });
+
+    const saveBtn = document.querySelector('#save-btn')
+    saveBtn.addEventListener('click', () => {
+        editor.save().then((data) => {
+            savedData = data
+        })
+    })
 }
 
-function createLists(data){
+function createLists(data) {
 
-    const sorted = data.bartenders.sort((a, b) =>  a.name < b.name ? -1 : 1)
+    const sorted = data.bartenders.sort((a, b) => a.name < b.name ? -1 : 1)
     sorted.forEach(appendTable)
 
     createStockChart(data.storage)
@@ -83,56 +115,57 @@ function showNotification(message) {
         position: "right",
         backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
         stopOnFocus: true,
-        onClick: function(){} // Callback after click
-      }).showToast();
+        onClick: function () {} // Callback after click
+    }).showToast();
 
 }
 
-function appendTapList (item) {
+function appendTapList(item) {
     const copy = document.querySelector('#taps template').content.cloneNode(true)
 
     copy.querySelector('.tap-name').textContent = `${item.id+1}: ${item.beer}`
     const percent = Math.floor(item.level / item.capacity * 100)
-    copy.querySelector('.tap-percent').textContent =  percent + '%'
+    copy.querySelector('.tap-percent').textContent = percent + '%'
     copy.querySelector('.progress').style.width = percent + '%'
-    if(percent < 20) {
+    if (percent < 20) {
         copy.querySelector('.progress').style.background = 'red'
     }
 
     document.querySelector('.tap-information').appendChild(copy)
 }
+
 function appendTable(person) {
     const copy = document.querySelector('#employees').content.cloneNode(true)
 
     copy.querySelector('[data-info=name]').textContent = capitalize(person.name)
     copy.querySelector('[data-info=status]').textContent = capitalize(person.status)
-    copy.querySelector('[data-info=serving]').textContent = person.servingCustomer ?  'ID: ' + person.servingCustomer : 'NA'
+    copy.querySelector('[data-info=serving]').textContent = person.servingCustomer ? 'ID: ' + person.servingCustomer : 'NA'
     console.log(person.statusDetail)
     let status = "";
-    if (person.statusDetail === "pourBeer"){
+    if (person.statusDetail === "pourBeer") {
         status = "Pouring beer"
-    } else if(person.statusDetail === "releaseTap"){
+    } else if (person.statusDetail === "releaseTap") {
         status = "Reserving tap"
-    } else if(person.statusDetail === "replaceKeg"){
+    } else if (person.statusDetail === "replaceKeg") {
         status = "Replacing keg"
-    } else if(person.statusDetail === "receivePayment") {
+    } else if (person.statusDetail === "receivePayment") {
         status = "Receiving payment"
-    } else if(person.statusDetail === "startServing") {
+    } else if (person.statusDetail === "startServing") {
         status = "Starting serving"
     }
 
     console.log(status)
     copy.querySelector('[data-info=status-detail]').textContent = status
-    copy.querySelector('[data-info=using-tap]').textContent = person.usingTap ?  person.usingTap : 'NA'
+    copy.querySelector('[data-info=using-tap]').textContent = person.usingTap ? person.usingTap : 'NA'
 
     document.querySelector('#staff tbody').appendChild(copy)
 }
 
 function createStockChart(information) {
 
-    const amount = information.map(number=>number.amount)
+    const amount = information.map(number => number.amount)
 
-    const names = information.map(string=>string.name)
+    const names = information.map(string => string.name)
 
     const labels = names
 
@@ -176,20 +209,20 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase()
 }
 
-function getStock(data){
-    return data.filter(string=>string.amount <= 2).length
+function getStock(data) {
+    return data.filter(string => string.amount <= 2).length
 }
 
-function getTap(data){
-    return data.filter(string=>string.level <= 600).length
+function getTap(data) {
+    return data.filter(string => string.level <= 600).length
 }
 
-function insertData(id, data){
+function insertData(id, data) {
     document.querySelector(`#${id} h2`).textContent = data
 }
 
-function insertStock(id, data){
-    if(data){
+function insertStock(id, data) {
+    if (data) {
         document.querySelector(`#${id} h2`).textContent = data
     } else {
         document.querySelector(`#${id} h2`).textContent = 'No Items'
--- js/orders.js
+++ js/orders.js
@@ -4,36 +4,44 @@ import '../css/main.scss'
 import '@fortawesome/fontawesome-free/js/all.min.js'
 
 const list = [{
-    "id": 1,
-    "startTime": 1622291201602,
-    "order": [
-      "El Hefe",
-      "Hollaback Lager",
-      "Sleighride",
-      "Steampunk"
-    ],
-    "status" : 'Completed'
-  },{
-    "id": 3,
-    "startTime": 1622291321602,
-    "order": [
-      "El Hefe",
-      "Hollaback Lager",
-      "Sleighride",
-      "Steampunk"
-    ],
-    "status" : 'Completed'
-  },{
-    "id": 5,
-    "startTime": 1622291741602,
-    "order": [
-      "El Hefe",
-      "Hollaback Lager",
-      "Sleighride",
-      "Steampunk"
-    ],
-    "status" : 'Completed'
-  }]
+  "id": 1,
+  "startTime": 1622291201602,
+  "price": 80,
+  "order": [
+    "El Hefe",
+    "Hollaback Lager",
+    "Sleighride",
+    "Steampunk"
+  ],
+  "status": 'Completed'
+}, {
+  "id": 3,
+  "startTime": 1622291321602,
+  "price": 120,
+  "order": [
+    "El Hefe",
+    "Hollaback Lager",
+    "Sleighride",
+    "Steampunk"
+  ],
+  "status": 'Completed'
+}, {
+  "id": 5,
+  "startTime": 1622291741602,
+  "price": 180,
+  "order": [
+    "El Hefe",
+    "Hollaback Lager",
+    "Sleighride",
+    "Steampunk"
+  ],
+  "status": 'Completed'
+}]
+
+const settings = {
+  sortBy: 'id',
+  sortDirection: 'asc',
+}
 
 window.addEventListener('DOMContentLoaded', init)
 
@@ -41,19 +49,22 @@ function init() {
 
 
 
-    selectButtons()
-    loadData()
+  selectButtons()
+  loadData()
 }
 
 function selectButtons() {
-    document.querySelectorAll('button').forEach(element => {
-        element.addEventListener('click', filterList)
-    });
+  document.querySelectorAll('button').forEach(element => {
+    element.addEventListener('click', filterList)
+  });
+  document.querySelectorAll('[data-action=sort]').forEach(button => {
+    console.log(2222)
+    button.addEventListener('click', selectSort)
+  })
 }
 
 
-async function loadData()
-{
+async function loadData() {
   let response = await fetch('https://foobardata.herokuapp.com');
   let data = await response.json()
   handleData(data)
@@ -61,63 +72,139 @@ async function loadData()
 
 
 
-function handleData(data){
+function handleData(data) {
 
-    const mappedQueue = data.queue.map(setStatus, 'q')
-    const mappedServing = data.serving.map(setStatus, 's')
-    list.push(...mappedQueue)
-    list.push(...mappedServing)
+  const mappedQueue = data.queue.map(setStatus, 'q')
+  const mappedServing = data.serving.map(setStatus, 's')
+  list.push(...mappedQueue)
+  list.push(...mappedServing)
 
-    handleList(list)
-    console.log(list.filter(string => string.status === 'Queue').length)
-    insertData('queue' , list.filter(string => string.status === 'Queue').length)
-    insertData('serving' , list.filter(string => string.status === 'Serving').length)
-    insertData('completed' , list.filter(string => string.status === 'Completed').length)
+  handleList(list)
+  console.log(list.filter(string => string.status === 'Queue').length)
+  insertData('queue', list.filter(string => string.status === 'Queue').length)
+  insertData('serving', list.filter(string => string.status === 'Serving').length)
+  insertData('completed', list.filter(string => string.status === 'Completed').length)
 
 }
 
-function insertData(id, data){
+
+function selectSort(event) {
+
+
+
+  const sortDirection = event.target.dataset.sortDirection;
+  const sortBy = event.target.dataset.sort;
+  console.log(sortBy)
+
+  const old = document.querySelector(`[data-sort='${settings.sortBy}']`)
+
+  old.classList.remove('chosen')
+  event.target.classList.add('chosen')
+
+
+  if (sortDirection === 'asc') {
+    event.target.dataset.sortDirection = 'desc'
+  } else {
+    event.target.dataset.sortDirection = 'asc'
+  }
+
+  setSort(sortBy, sortDirection)
+
+}
+
+function setSort(sortBy, sortDirection) {
+  settings.sortBy = sortBy
+  settings.sortDirection = sortDirection
+  document.querySelector('tbody').innerHTML = ''
+  handleList(list)
+}
+
+function sortList(list) {
+
+  let direction = 1
+
+  if (settings.sortDirection === 'desc') {
+    direction = -1
+  } else {
+    direction = 1
+  }
+
+  const sortedList = list.sort(sortProperty)
+  console.log(settings.sortBy)
+  function sortProperty(a, b) {
+    if (a[settings.sortBy] < b[settings.sortBy]) {
+      return -1 * direction
+    } else {
+      return 1 * direction
+    }
+  }
+
+  console.log(sortedList)
+
+  return sortedList
+
+}
+
+function sortLoadedList(a, b) {
+
+  if (a.id > b.id) {
+    return 1
+  }
+  if (b.id> a.id) {
+    return -1
+  } else {
+    return 0
+  }
+
+}
+
+function insertData(id, data) {
   document.querySelector(`#${id} h2`).textContent = data
 }
 
 function setStatus(string, s) {
-    s ? string.status = 'Queue' : string.status = 'Serving'
-    return string
+  const randomPrices = [20,25,30,35,40,45,50]
+  const price = randomPrices[Math.floor(Math.random()*randomPrices.length)]
+  string.price = string.order.length * price
+  s ? string.status = 'Queue' : string.status = 'Serving'
+  console.log(string)
+  return string
 }
 
-function handleList(list){
-    list.sort((a, b) => a.id - b.id)
-
-    list.forEach(createTable)
+function handleList(currentList) {
+  const sortedList = sortList(currentList)
+  console.log(sortedList)
+  sortedList.forEach(createTable)
 
 
 }
 
 function createTable(item) {
-    const copy = document.querySelector('template').content.cloneNode(true)
+  const copy = document.querySelector('template').content.cloneNode(true)
 
-    copy.querySelector('[data-info=id]').textContent = item.id
-    copy.querySelector('[data-info=start-time]').textContent = getTime(item.startTime)
-    item.order.forEach(element=>{
-        const li = document.createElement('li')
-        li.textContent = element
-        copy.querySelector('[data-info=order] ul').appendChild(li)
-    })
-    copy.querySelector('[data-info=total]').textContent = 20
+  copy.querySelector('[data-info=id]').textContent = item.id
+  copy.querySelector('[data-info=start-time]').textContent = getTime(item.startTime)
+  item.order.forEach(element => {
+    const li = document.createElement('li')
+    li.textContent = element
+    copy.querySelector('[data-info=order] ul').appendChild(li)
+  })
+  console.log(item.price)
+  copy.querySelector('[data-info=total]').textContent = item.price
 
-    const statusElement = copy.querySelector('[data-info=status] span')
+  const statusElement = copy.querySelector('[data-info=status] span')
 
-    statusElement.textContent = item.status
+  statusElement.textContent = item.status
 
-    switch(item.status) {
-      case 'Completed':
-        statusElement.style.background = 'rgb(35, 174, 137)'
-        break;
-      case 'Queue':
-        statusElement.style.background = 'rgb(249, 142, 51)'
-    }
+  switch (item.status) {
+    case 'Completed':
+      statusElement.style.background = 'rgb(35, 174, 137)'
+      break;
+    case 'Queue':
+      statusElement.style.background = 'rgb(249, 142, 51)'
+  }
 
-    document.querySelector('.section-table tbody').appendChild(copy)
+  document.querySelector('.section-table tbody').appendChild(copy)
 }
 
 function getTime(time) {
@@ -134,24 +221,24 @@ function getTime(time) {
 }
 
 function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase()
+  return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase()
 }
 
-function filterList (event) {
-    event.preventDefault()
-    let filteredList = []
-    document.querySelectorAll('button').forEach(e=>{
-      e.style.background = '#1C7EBB';
-    })
-    event.target.style.background = 'red'
-    const filterBy = event.target.dataset.filter
+function filterList(event) {
+  event.preventDefault()
+  let filteredList = []
+  document.querySelectorAll('button').forEach(e => {
+    e.style.background = '#1C7EBB';
+  })
+  event.target.style.background = 'red'
+  const filterBy = event.target.dataset.filter
 
-    if (filterBy == '*') {
-        filteredList = list
-    } else {
+  if (filterBy == '*') {
+    filteredList = list
+  } else {
 
-        filteredList = list.filter(string=>string.status==filterBy)
-    }
-    document.querySelector('.section-table tbody').innerHTML = ''
-    handleList(filteredList)
+    filteredList = list.filter(string => string.status == filterBy)
+  }
+  document.querySelector('.section-table tbody').innerHTML = ''
+  handleList(filteredList)
 }
